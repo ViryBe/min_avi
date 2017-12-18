49942190b74029eb60841e3948756dfde6a4d73f
gabrielhdt <gabrielhondet@gmail.com>
2017-12-18 14:52:01 +0100

    (~) changed name

--- sidestick/test/test_inp.py
+++ sidestick/test/test_inp.py
@@ -15,40 +15,19 @@ def on_die_proc(agent, _id):
 
 
 def on_msg(agent, data):
-    global nz_sent
-    global p_sent
     nz_sent = dr.nz_from_stick(_js)
     p_sent = dr.p_from_stick(_js)
     isa.IvySendMsg("APNzCommand nz={0}".format(nz_sent))
-    isa.IvySendMsg("APLatCommand rollRate={0}".format(p_sent))
-
-
-def cmp_float(x, y):
-    epsilon = 1e-5
-    return abs(x - y) < epsilon
+    isa.IvySendMsg("APLatCommand p={0}".format(p_sent))
 
 
 def reception_nz(agent, nz_received):
-    global nz_sent
-    if nz_sent >= -1 and nz_sent <= 2.5:
-        print (nz_sent, float(nz_received),
-               cmp_float(nz_sent, float(nz_received)))
-    elif nz_sent < -1:
-        print (nz_sent, float(nz_received), cmp_float(-1, float(nz_received)))
-    else:
-        print (nz_sent, float(nz_received), cmp_float(2.5, float(nz_received)))
+    print("nz: {}".format(nz_received))
 
 
 def reception_p(agent, p_received):
-    global p_sent
-    if p_sent >= -15 * _common.DEG2RAD and p_sent <= 15 * _common.DEG2RAD:
-        print (p_sent, float(p_received), cmp_float(p_sent, float(p_received)))
-    elif p_sent < -15 * _common.DEG2RAD:
-        print (p_sent, float(p_received),
-               cmp_float(-15 * _common.DEG2RAD, float(p_received)))
-    else:
-        print (p_sent, float(p_received),
-               cmp_float(15 * _common.DEG2RAD, float(p_received)))
+    print("p: {}".format(p_received))
+
 
 _js = dr.init_js()
 app_name = "MyIvyApplication"
